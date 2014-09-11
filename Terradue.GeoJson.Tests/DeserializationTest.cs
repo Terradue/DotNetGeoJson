d8bd95554b1ebf41b9ecc21bcc1223ea42647387
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-09-11 17:44:14 +0200

    some more tests
    Signed-off-by: Emmanuel Mathot <emmanuel.mahot@gmail.com>

---
+++ Terradue.GeoJson.Tests/DeserializationTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using ServiceStack.Text;
+using Terradue.GeoJson.Feature;
+
+namespace Terradue.GeoJson.Tests {
+    [TestFixture()]
+    public class DeserializationTest {
+
+        [Test()]
+        public void MultiPolygonDeserialization() {
+
+            FileStream fs = new FileStream("../MultiPolygon.geojson", FileMode.Open);
+
+            Terradue.GeoJson.Feature.Feature f = JsonSerializer.DeserializeFromStream<Terradue.GeoJson.Feature.Feature>(fs);
+            fs.Close();
+
+            Assert.True(f is MultiPolygonFeature);
+
+        }
+    }
+}
+
