962babdb1b1c906dbd34ca47b36400b9c015eb4a
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2014-11-26 18:25:05 +0100

    GeoRSS first implementation
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

---
+++ Terradue.GeoJson.Tests/GeoRSSTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Xml;
+using Terradue.GeoJson.Geometry;
+using Terradue.GeoJson.Feature;
+
+namespace Terradue.GeoJson.Tests { 
+
+    [TestFixture()]
+    public class GeoRSSTest {
+
+        [Test()]
+        public void PolygonTestCase() {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../polygon.georss");
+
+            XmlElement e = doc.DocumentElement;
+
+            var geom = GeometryFactory.GeoRSSToGeometry(e);
+
+            Assert.IsTrue(geom is Polygon);
+
+        }
+    }
+}
+
