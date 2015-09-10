c3d514700189f9399a6b58031fb4eeb348a8a587
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-09-10 22:04:31 +0200

    bug split fixed
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.GeoJson.Tests/SplitTests.cs
+++ Terradue.GeoJson.Tests/SplitTests.cs
@@ -28,6 +28,15 @@ namespace Terradue.GeoJson.Tests {
             newmpoly = mpoly.ToWkt();
 
             Assert.AreEqual("POLYGON((160 45,170 45,175 45,178 40,174 40,160 45))", newmpoly);
+
+            var mpoly2 = GeometryFactory.MultiPolygonFromWKT("MULTIPOLYGON(((138.141861 83.754265,149.33284 87.420456,-166.712769 84.741585,168.046951 82.234276,138.141861 83.754265)))");
+
+            mpoly = GeometryFactory.SplitWorldExtent(mpoly2);
+
+            newmpoly = mpoly.ToWkt();
+
+            Assert.AreEqual("MULTIPOLYGON(((138.141861 83.754265,149.33284 87.420456,180 85.5513966476281,180 83.4216632768107,168.046951 82.234276,138.141861 83.754265)),((-180 85.5513966476281,-166.712769 84.741585,-180 83.4216632768107)))", newmpoly);
+
         }
 
     }
