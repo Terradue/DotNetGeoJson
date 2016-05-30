49295d7b350baed1e8d82d4d70ab2964d873bfd1
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-05-30 16:19:34 +0200

    bump version to 1.7.1
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.GeoJson/Properties/AssemblyInfo.cs
+++ Terradue.GeoJson/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@
 @{
     Terradue .NET GeoJson Library. Initially developed to provide an easy way to manage Geometry objects with serialization and deserialization functions and transformation functions from/to GeoJson, it also supports GML, georss and Well Known Text (WKT)
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.7.0
+    \xrefitem sw_version "Versions" "Software Package Version" 1.7.1
 
     \xrefitem sw_link "Links" "Software Package List" [DotNetGeoJson](https://github.com/Terradue/DotNetGeoJson)
 
@@ -44,6 +44,6 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNetGeoJson")]
 [assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNetGeoJson/blob/master/LICENSE")]
 [assembly: AssemblyCopyright("Terradue")]
-[assembly: AssemblyVersion("1.7.0.*")]
-[assembly: AssemblyInformationalVersion("1.7.0")]
+[assembly: AssemblyVersion("1.7.1.*")]
+[assembly: AssemblyInformationalVersion("1.7.1")
                                                               
