f7d6a25aada065a6554b58b4257d01fbd3c62aec
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-08-03 14:25:49 +0200

    e
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.GeoJson/Terradue/GeoJson/GeoRss/GeoRss.cs
+++ Terradue.GeoJson/Terradue/GeoJson/GeoRss/GeoRss.cs
@@ -51,7 +51,7 @@ namespace Terradue.GeoJson.GeoRss {
         [System.Xml.Serialization.XmlElementAttribute("LineString", typeof(LineStringType), Namespace = "http://www.opengis.net/gml")]
         [System.Xml.Serialization.XmlElementAttribute("Point", typeof(PointType), Namespace = "http://www.opengis.net/gml")]
         [System.Xml.Serialization.XmlElementAttribute("Polygon", typeof(PolygonType), Namespace = "http://www.opengis.net/gml")]
-        [System.Xml.Serialization.XmlAnyElement(Namespace = "http://www.opengis.net/gml/3.2")]
+        [System.Xml.Serialization.XmlAnyElement(Namespace = "http://www.opengis.net/gml")]
         public object[] Item {
             get {
                 return this.itemField;
